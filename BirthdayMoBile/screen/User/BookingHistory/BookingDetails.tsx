79fe2156d9659833e8b955389ac09a713a13fa98
An Vu <hoanganwm@gmail.com>
2024-04-23 14:57:38 +0700

    history-list

--- BirthdayMoBile/screen/User/BookingHistory/BookingDetails.tsx
+++ BirthdayMoBile/screen/User/BookingHistory/BookingDetails.tsx
@@ -1,13 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
-// Ân
-
+function getApiUrl(id: string): string {
+  return `https://party-renting-platform-aa30573d1765.herokuapp.com/api/bookings/customer/1${id}`;
+}
 
 export default function BookingDetails() {
   return (
     <View>
       <Text>BookingDetails</Text>
     </View>
-  )
+  );
 }
